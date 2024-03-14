d051b35f8d11a7573f9ce118999b7a90b0d048f7
Dmytro <dimalimaredmi@gmail.com>
2024-03-14 21:55:04 +0200

    24-abstract class

--- dist/importantInterface.js
+++ dist/importantInterface.js
@@ -1,20 +1,28 @@
 "use strict";
-class Instagram {
-    constructor(cameraMode, filter, burst) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-    }
-}
-class YouTube {
-    constructor(cameraMode, filter, burst) {
-        this.cameraMode = cameraMode;
-        this.filter = filter;
-        this.burst = burst;
-    }
-    simpleMethod() {
-        console.log("Yeeee, you did it, maaaaan!");
-    }
-}
-const newYoutubeUser = new YouTube("8px", "HD", 10);
-newYoutubeUser.simpleMethod();
+// interface TakePhoto {
+//   cameraMode: string;
+//   filter: string;
+//   burst: number;
+// }
+// // class Instagram implements TakePhoto {
+// //   constructor(
+// //     public cameraMode: string,
+// //     public filter: string,
+// //     public burst: number
+// //   ) {}
+// // }
+// interface Story {
+//   simpleMethod(): void;
+// }
+// class YouTube implements TakePhoto, Story {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {}
+//   simpleMethod(): void {
+//     console.log("Yeeee, you did it, maaaaan!");
+//   }
+// }
+// const newYoutubeUser = new YouTube("8px", "HD", 10);
+// newYoutubeUser.simpleMethod();
--- src/importantInterface.ts
+++ src/importantInterface.ts
@@ -1,32 +1,32 @@
-interface TakePhoto {
-  cameraMode: string;
-  filter: string;
-  burst: number;
-}
+// interface TakePhoto {
+//   cameraMode: string;
+//   filter: string;
+//   burst: number;
+// }
 
-class Instagram implements TakePhoto {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number
-  ) {}
-}
+// // class Instagram implements TakePhoto {
+// //   constructor(
+// //     public cameraMode: string,
+// //     public filter: string,
+// //     public burst: number
+// //   ) {}
+// // }
 
-interface Story {
-  simpleMethod(): void;
-}
+// interface Story {
+//   simpleMethod(): void;
+// }
 
-class YouTube implements TakePhoto, Story {
-  constructor(
-    public cameraMode: string,
-    public filter: string,
-    public burst: number
-  ) {}
+// class YouTube implements TakePhoto, Story {
+//   constructor(
+//     public cameraMode: string,
+//     public filter: string,
+//     public burst: number
+//   ) {}
 
-  simpleMethod(): void {
-    console.log("Yeeee, you did it, maaaaan!");
-  }
-}
+//   simpleMethod(): void {
+//     console.log("Yeeee, you did it, maaaaan!");
+//   }
+// }
 
-const newYoutubeUser = new YouTube("8px", "HD", 10);
-newYoutubeUser.simpleMethod();
+// const newYoutubeUser = new YouTube("8px", "HD", 10);
+// newYoutubeUser.simpleMethod();
