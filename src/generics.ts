82a25d137edbfea16e55bcc838ad486106c061d1
Dmytro <dimalimaredmi@gmail.com>
2024-03-15 19:28:49 +0200

    25-generics

---
+++ src/generics.ts
@@ -0,0 +1,26 @@
+// function identityOne(val: boolean | number): boolean | number {
+//   return val;
+// }
+
+// function identityTwo(val: any): any {
+//   return val;
+// }
+
+// function identityThree<Type>(val: Type): Type {
+//   return val;
+// }
+
+// function identityFour<T>(val: T): T {
+//   return val;
+// }
+
+function getSearchProducts<T>(products: T[]): T {
+  // do some database operations
+  const myIndex = 3;
+  return products[myIndex];
+}
+
+const getMoreSearchProducts = <T>(products: T[], prices: number): T => {
+  // do some database operations
+  return products[prices];
+};
